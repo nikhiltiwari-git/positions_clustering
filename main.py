09ecbf6b294426455bccb19d814ed36587e72a06
nikhiltiwari-git <20001011043@jcboseust.ac.in>
2024-01-06 14:55:39 +0530

    Problem 2

--- main.py
+++ main.py
@@ -16,5 +16,27 @@ def combinedPositionTask1():
     combinedDf = pd.concat(dfs, ignore_index=True)
     combinedDf.to_csv('combined_closePosition.csv', index=False)
 
-combinedPositionTask1()
+
+def combinedStatsTask2():
+
+    combinedDf = pd.read_csv('combined_closePosition.csv')
+
+    totalTrades = len(combinedDf)
+    uniqueDays = len(combinedDf['Date'].unique())
+    averageTrades = totalTrades/ uniqueDays
+    totalPnl = combinedDf['Pnl'].sum()
+    profitTrades = (combinedDf['Pnl'] > 0).sum()
+    lossTrades = (combinedDf['Pnl'] <= 0).sum()
+
+    with open('combined_stats.txt', 'w') as file:
+        file.write(f"Total trades: {totalTrades}\n")
+        file.write(f"Unique days: {uniqueDays}\n")
+        file.write(f"Average trades: {int(averageTrades)}\n")
+        file.write(f"Total Pnl: {int(totalPnl)}\n")
+        file.write(f"Profit trades: {profitTrades}\n")
+        file.write(f"Loss trades: {lossTrades}\n")
+
+
+# combinedPositionTask1()
+combinedStatsTask2()
 
